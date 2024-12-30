92d3b4b13157d09c2d0b88c17a80a1fcf2e888d2
franciscoylderlan <ylderlan@nca.ufma.br>
2024-12-30 17:05:48 -0300

    ✨ feat: Add comments section

--- src/app/[slug]/play/page.tsx
+++ src/app/[slug]/play/page.tsx
@@ -1,5 +1,9 @@
 import { Suspense } from 'react';
 
+import { Comment } from '@/components/comment';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
 import { VideoCard } from '@/components/video-card';
 import VideoCardSkeleton from '@/components/video-card-skeleton';
 import { VideoPlayer } from '@/components/video-player';
@@ -21,34 +25,51 @@ export async function getVideoDetails(param: string) {
   return video;
 }
 
-export default async function Play({ params }: { params: { slug: string } }) {
+export default async function Play({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
   const videoDetails = await getVideoDetails(slug);
 
   return (
     <main className="container mx-auto px-4 py-6">
       <div className="flex flex-col gap-4 md:flex-row">
-        <div className="flex-1">
+        <div className="flex flex-1 flex-col gap-10">
           <div className="overflow-hidden rounded-lg">
             <VideoPlayer videoDetails={videoDetails.video} />
           </div>
-
-          <h2 className="mt-4 text-2xl font-bold text-primary">
-            {/* {video.title} */}
-          </h2>
-
-          <div className="mt-2 flex items-center justify-between text-secondary">
-            <Suspense
-              fallback={
-                <div className="mr-2 h-8 w-24 animate-pulse rounded bg-secondary"></div>
-              }
-            >
-              {/* <VideoLikeCounter videoId={video.id} /> */}
-            </Suspense>
+          <div>
+            <span className="text-2xl font-bold">
+              {videoDetails.commentsResponse.comments.length} Comentários
+            </span>
+            <Separator className="mb-4 mt-2" />
+            <div className="flex flex-col items-start gap-4">
+              <Input
+                type={'text'}
+                placeholder="Diga algo sobre isto ..."
+                className="bg-muted p-7 !text-lg hover:border-ring"
+              />
+              <Button className="flex items-center justify-center px-4 py-2 text-lg font-semibold">
+                Mandar
+              </Button>
+            </div>
           </div>
 
-          <div className="mt-2 rounded-lg bg-primary">
-            {/* <p className="text-primary">{video.description}</p> */}
+          <div>
+            {videoDetails.commentsResponse.comments.map(
+              ({ id, body, likes, postId, user }) => (
+                <Comment
+                  key={id}
+                  id={id}
+                  body={body}
+                  likes={likes}
+                  postId={postId}
+                  user={user}
+                />
+              )
+            )}
           </div>
         </div>
 
--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,11 +1,37 @@
-import { Comment as CommentProps } from '@/data/comments';
+import { MessageCircle, ThumbsUp, UserRound } from 'lucide-react';
 
-export function Comment({ id, postId, body, likes, user }: CommentProps) {
+import { Comment as CommentProps } from '@/interfaces/comment';
+
+import { Button } from './ui/button';
+
+export function Comment({ body, likes, user }: CommentProps) {
   return (
-    <div>
-      <h4>@{user.username}</h4>
-      <p>{body}</p>
-      <span></span>
+    <div className="mb-6 flex flex-col">
+      <div className="flex items-start gap-4">
+        <span className="rounded-full bg-muted-foreground">
+          <UserRound className="h-12 w-12 text-muted" />
+        </span>
+        <div className="flex flex-col gap-2">
+          <h4 className="font-semibold text-primary">@{user.username}</h4>
+          <p>{body}</p>
+          <div className="flex gap-2">
+            <Button
+              variant={'ghost'}
+              className="flex items-center justify-center"
+            >
+              <MessageCircle />
+              Responder comentário
+            </Button>
+            <Button
+              variant={'ghost'}
+              className="flex items-center justify-center"
+            >
+              <ThumbsUp className="text-primary" />
+              {likes}
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
